44928c8f2499435410db17ccb1c07c90d365ea2a
Simone Giordano <sg@simonegiordano.it>
2025-06-16 17:12:08 +0200

    Barcodes

---
+++ code/Barcode/Barcode.Codeunit.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Org.BouncyCastle.Cms;
+
+namespace BCRemoteFunctions
+{
+    public class Barcode : Codeunit
+    {
+        [ApiMethod(Method = RequestMethod.Post, Route = "barcode/GetQrCode")]
+        public string GetQrCode(string text, int pixelsPerModule)
+        {
+            var qrData = QRCoder.QRCodeGenerator.GenerateQrCode(text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.Base64QRCode(qrData);
+            return qrCode.GetGraphic(pixelsPerModule);
+        }
+    }
+}
---
+++ code/Generic/Test.Codeunit.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Org.BouncyCastle.Cms;
+
+namespace BCRemoteFunctions
+{
+    public class Test : Codeunit
+    {
+        [ApiMethod(Method = RequestMethod.Post, Route = "test/TestConnection")]
+        public bool TestConnection()
+        {
+            return true;
+        }
+    }
+}
